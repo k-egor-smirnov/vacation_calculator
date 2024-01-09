bc3763f4dd5b8a549989d0ae2e475617d3eda657
Egor Smirnov <e.smirnov@corp.vk.com>
2024-01-09 09:13:52 +0300

    add VK Mini Apps support

--- src/App.tsx
+++ src/App.tsx
@@ -20,65 +20,82 @@ import { formStore } from "./FormStore";
 import { BasicData } from "./panels/BasicData/BasicData";
 import { Dates } from "./panels/Dates/Dates";
 import { Result } from "./panels/Result/Result";
+import { useEffect } from "react";
+import bridge from "@vkontakte/vk-bridge";
+import { VK_RETARGETING_PIXEL_ID } from "./consants";
 
-const App = observer(() => {
+const Content = observer(() => {
+  return (
+    <SplitLayout header={<PanelHeader delimiter="none" />}>
+      <SplitCol autoSpaced>
+        <Root activeView={formStore.step === "intro" ? "intro" : "main"}>
+          <View nav="intro" activePanel="intro">
+            <Panel nav="intro" centered>
+              <Intro />
+            </Panel>
+          </View>
+          <View nav="main" activePanel={formStore.step}>
+            <Panel nav="dates" style={{ paddingBottom: 60 }}>
+              <Dates />
+              <div style={{ height: 68 }}></div>
+            </Panel>
+            <Panel nav="basic">
+              <BasicData />
+              <div style={{ height: 68 }}></div>
+            </Panel>
+            <Panel nav="result">
+              <Result />
+              <div style={{ height: 68 }}></div>
+            </Panel>
+          </View>
+        </Root>
+        {formStore.step !== "intro" && (
+          <FixedLayout vertical="bottom" filled>
+            <Div>
+              <ButtonGroup stretched>
+                <Button
+                  stretched
+                  size="l"
+                  mode="secondary"
+                  onClick={() => formStore.prevStep()}
+                >
+                  Назад
+                </Button>
+                <Button
+                  stretched
+                  size="l"
+                  disabled={!formStore.isNextStepAvailable}
+                  onClick={() => formStore.nextStep()}
+                >
+                  Дальше
+                </Button>
+              </ButtonGroup>
+            </Div>
+          </FixedLayout>
+        )}
+      </SplitCol>
+    </SplitLayout>
+  );
+});
+
+function App() {
+  useEffect(() => {
+    bridge.send("VKWebAppRetargetingPixel", {
+      pixel_code: VK_RETARGETING_PIXEL_ID,
+      event: "page_view",
+    });
+    bridge.send("VKWebAppInit");
+  }, []);
+
   return (
     <ConfigProvider>
       <AdaptivityProvider>
         <AppRoot>
-          <SplitLayout header={<PanelHeader delimiter="none" />}>
-            <SplitCol autoSpaced>
-              <Root activeView={formStore.step === "intro" ? "intro" : "main"}>
-                <View nav="intro" activePanel="intro">
-                  <Panel nav="intro" centered>
-                    <Intro />
-                  </Panel>
-                </View>
-                <View nav="main" activePanel={formStore.step}>
-                  <Panel nav="dates" style={{ paddingBottom: 60 }}>
-                    <Dates />
-                    <div style={{ height: 68 }}></div>
-                  </Panel>
-                  <Panel nav="basic">
-                    <BasicData />
-                    <div style={{ height: 68 }}></div>
-                  </Panel>
-                  <Panel nav="result">
-                    <Result />
-                    <div style={{ height: 68 }}></div>
-                  </Panel>
-                </View>
-              </Root>
-              {formStore.step !== "intro" && (
-                <FixedLayout vertical="bottom" filled>
-                  <Div>
-                    <ButtonGroup stretched>
-                      <Button
-                        stretched
-                        size="l"
-                        mode="secondary"
-                        onClick={() => formStore.prevStep()}
-                      >
-                        Назад
-                      </Button>
-                      <Button
-                        stretched
-                        size="l"
-                        disabled={!formStore.isNextStepAvailable}
-                        onClick={() => formStore.nextStep()}
-                      >
-                        Дальше
-                      </Button>
-                    </ButtonGroup>
-                  </Div>
-                </FixedLayout>
-              )}
-            </SplitCol>
-          </SplitLayout>
+          <Content />
         </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
   );
-});
+}
 
 export default App;
